690f6f83e162618f38ea548f562aa41c0e123110
Marcus Machado <marcus15101996@gmail.com>
2024-07-14 23:56:46 -0300

    exercicios_024_025
    Registro das atividades desenvolvidas nos exercícios

---
+++ python_exercicios/ex024.py
@@ -0,0 +1,3 @@
+cid = str(input('Em que cidade você nasceu? ')).strip()
+print(cid[:18].upper() == 'BALNEARIO CAMBORIU')
+
---
+++ python_exercicios/ex025.py
@@ -0,0 +1,3 @@
+nome = str(input('Qual seu nome completo ? \n'))
+print('Seu nome tem Machado ? {}'. format('MACHADO' in nome.upper()))
+
